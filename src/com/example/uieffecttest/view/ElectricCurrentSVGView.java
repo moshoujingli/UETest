ac01343740a1e7dbae45c03357577820fbe38b0d
BiXiaopeng <bixiaopeng2007@gmail.com>
2015-04-24 23:46:34 +0900

    Initial commit

---
+++ src/com/example/uieffecttest/view/ElectricCurrentSVGView.java
@@ -0,0 +1,14 @@
+package com.example.uieffecttest.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class ElectricCurrentSVGView extends View {
+
+  public ElectricCurrentSVGView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    // TODO Auto-generated constructor stub
+  }
+
+}
