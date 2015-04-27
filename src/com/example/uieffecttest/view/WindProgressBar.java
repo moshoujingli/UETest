952a0039fb72004401c48fb077137842dd9c141a
BiXiaopeng <bixiaopeng2007@gmail.com>
2015-04-27 23:51:06 +0900

    add:interface for progress bar

--- src/com/example/uieffecttest/view/WindProgressBar.java
+++ src/com/example/uieffecttest/view/WindProgressBar.java
@@ -6,9 +6,39 @@ import android.widget.ProgressBar;
 
 public class WindProgressBar extends ProgressBar {
 
+  private final int DEFAULT_LEAF_PERCENT=5;
+  private final int DEFAULT_ANIM_DURATION=200;
+  
   public WindProgressBar(Context context, AttributeSet attrs) {
     super(context, attrs);
-    this.setIndeterminate(false);
+    this.setAnimationDuration(DEFAULT_ANIM_DURATION);
+    this.setPercentForOneLeaf(DEFAULT_LEAF_PERCENT);
+  }
+
+  @Override
+  public synchronized void setProgress(int progress) {
+    // TODO Auto-generated method stub
+  }
+
+  /**
+   * <p>
+   * Set the increment for each leaf.
+   * </p>
+   *
+   * @param percent the increment in percent for each leaf, between 0 and 100
+   */
+  public void setPercentForOneLeaf(int percent) {
+
+  }
+  /**
+   * <p>
+   * Set the progress increment animation duration.
+   * </p>
+   *
+   * @param duration the progress increment animation duration in millisecond.
+   */
+  public synchronized void setAnimationDuration(long duration) {
+
   }
 
 }
