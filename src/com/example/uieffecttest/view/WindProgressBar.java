d2bbd3f24b38dab7d04e448b136e6b68bde6df63
BiXiaopeng <bixiaopeng2007@gmail.com>
2015-05-03 23:33:35 +0900

    add[]:members

--- src/com/example/uieffecttest/view/WindProgressBar.java
+++ src/com/example/uieffecttest/view/WindProgressBar.java
@@ -6,18 +6,25 @@ import android.widget.ProgressBar;
 
 public class WindProgressBar extends ProgressBar {
 
-  private final int DEFAULT_LEAF_PERCENT=5;
-  private final int DEFAULT_ANIM_DURATION=200;
-  
+  private final int DEFAULT_LEAF_PERCENT = 5;
+  private final int DEFAULT_ANIM_DURATION = 200;
+  private final double DEFAULT_FAN_ANIM_SPEED = Math.PI * 2;
+
+  private int mLeafPercent;
+  private long mAnimDuration;
+  private double mFanAnimSpeed;
+
+
+
   public WindProgressBar(Context context, AttributeSet attrs) {
     super(context, attrs);
     this.setAnimationDuration(DEFAULT_ANIM_DURATION);
     this.setPercentForOneLeaf(DEFAULT_LEAF_PERCENT);
+    this.setFanSpeed(DEFAULT_FAN_ANIM_SPEED);
   }
 
   @Override
   public synchronized void setProgress(int progress) {
-    // TODO Auto-generated method stub
   }
 
   /**
@@ -28,8 +35,9 @@ public class WindProgressBar extends ProgressBar {
    * @param percent the increment in percent for each leaf, between 0 and 100
    */
   public void setPercentForOneLeaf(int percent) {
-
+    this.mLeafPercent = percent;
   }
+
   /**
    * <p>
    * Set the progress increment animation duration.
@@ -38,7 +46,24 @@ public class WindProgressBar extends ProgressBar {
    * @param duration the progress increment animation duration in millisecond.
    */
   public synchronized void setAnimationDuration(long duration) {
-
+    this.mAnimDuration=duration;
   }
 
+  /**
+   * <p>
+   * Set the speed for fan.
+   * </p>
+   *
+   * @param speed in arc.
+   */
+  public synchronized void setFanSpeed(double speed) {
+    this.mFanAnimSpeed=speed;
+  }
+
+  @Override
+  protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    // TODO Auto-generated method stub
+    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+  }
+    
 }
